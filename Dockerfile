710bf0a3e7392ce05adeae39f32ab0e0c2a32190
Vitor Araujo Boufleur <v.boufleur@gmail.com>
2018-02-21 10:48:28 -0300

    Use simpler alpine apache-php docker image to host speedtest

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,16 @@
-FROM webdevops/php-apache:alpine
+#Based on https://github.com/kost/docker-alpine/tree/master/alpine-apache-php
+FROM alpine:3.7
+
 ADD ./root /
+
+RUN apk add --update apache2 php7-apache2 && rm -f /var/cache/apk/* && \
+chown -R apache:apache /app && \
+chmod -R 755 /scripts
+
+#Add code to redirect apache logs to /dev/stdout and /dev/stderr
+
+EXPOSE 8000
+
+WORKDIR /app
+
+ENTRYPOINT ["/scripts/run.sh"]
---
+++ root/scripts/run.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+# execute any pre-init scripts, useful for images
+# based on this image
+for i in /scripts/pre-init.d/*sh
+do
+	if [ -e "${i}" ]; then
+		echo "[i] pre-init.d - processing $i"
+		. "${i}"
+	fi
+done
+
+# set apache as owner/group
+if [ "$FIX_OWNERSHIP" != "" ]; then
+	chown -R apache:apache /app
+fi
+
+# display logs
+#tail -F /var/log/apache2/*log &
+
+# execute any pre-exec scripts, useful for images
+# based on this image
+for i in /scripts/pre-exec.d/*sh
+do
+	if [ -e "${i}" ]; then
+		echo "[i] pre-exec.d - processing $i"
+		. "${i}"
+	fi
+done
+
+echo "[i] Starting daemon..."
+# run apache httpd daemon
+httpd -X
